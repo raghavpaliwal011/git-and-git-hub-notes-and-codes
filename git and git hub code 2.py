8b7e88ae37e3f3de886e1ed9df97beeb9ad9ddb2
raghav <raghavpaliwal1109@gmail.com>
2023-12-25 16:35:29 +0530

    added more codes

---
+++ git and git hub code 2.py
@@ -0,0 +1,5 @@
+# this code is just for example 
+
+name = input("enter your name: ")
+greetings = (f"good morning {name}")
+print (greetings)
---
+++ git and git hub code 3.py
@@ -0,0 +1,10 @@
+# this code is just for example
+
+print ("a = 5")
+print ("b = 89")
+question = input("what is the difference between a and b : ")
+if question == "94":
+    print("correct")
+
+else:
+    print ("you are weak maths nooooooooooooooooob")
