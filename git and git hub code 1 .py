c99490f17920012598574ab7f56a0bb7456c4de8
raghav <raghavpaliwal1109@gmail.com>
2023-12-25 16:24:26 +0530

    first commit in git

---
+++ git and git hub code 1 .py
@@ -0,0 +1,8 @@
+# this code is just for example
+
+a = input("enter a number : ")
+b = input("enter a number : ")
+a = int(a)
+b = int(b)
+sum_of_a_and_b = ("the sum of a and b is equal to", a+b)
+print (sum_of_a_and_b)
